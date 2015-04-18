e03939c35f472b3a113de4d08a58044902c5327c
fsb4000 <fsb4000@yandex.ru>
2015-04-18 19:04:44 +0600

    delete obsolete warning

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -25,9 +25,9 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         (     0, hashGenesisBlockOfficial )
-		(     10000,uint256("0x0000000000b780717105a4a6fbfc31c4439e8ff7cffaafacfcf92aac5198ebdb"))
-		(     26722,uint256("0xc7234cbc7e1ea9e8edaec094055a55b9b3d76a4c209424d697e4b1578e732346"))
-		(     536310,uint256("0x339a9a3edefed2570629cb8e634101776c9c42fca7cb6b19c9001f715c679f12"))
+        (     10000,uint256("0x0000000000b780717105a4a6fbfc31c4439e8ff7cffaafacfcf92aac5198ebdb"))
+        (     26722,uint256("0xc7234cbc7e1ea9e8edaec094055a55b9b3d76a4c209424d697e4b1578e732346"))
+        (     536310,uint256("0x339a9a3edefed2570629cb8e634101776c9c42fca7cb6b19c9001f715c679f12"))
 		;
 
     static MapCheckpoints mapCheckpointsTestnet =
@@ -354,15 +354,6 @@ namespace Checkpoints
                 pindexSync->GetBlockTime() + nStakeMinAge < GetAdjustedTime());
     }
 
-    // Is the sync-checkpoint too old?
-    bool IsSyncCheckpointTooOld(unsigned int nSeconds)
-    {
-        LOCK(cs_hashSyncCheckpoint);
-        // sync-checkpoint should always be accepted block
-        assert(mapBlockIndex.count(hashSyncCheckpoint));
-        const CBlockIndex* pindexSync = mapBlockIndex[hashSyncCheckpoint];
-        return (pindexSync->GetBlockTime() + nSeconds < GetAdjustedTime());
-    }
 }
 
 // ppcoin: sync-checkpoint master key
